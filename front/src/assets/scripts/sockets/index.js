49d333929430ccc4a951e0e64d5c6b3d9e76351a
dgekaa <dgeka_vip@mail.ru>
2021-01-25 15:54:53 +0300

    server

--- front/src/assets/scripts/sockets/index.js
+++ front/src/assets/scripts/sockets/index.js
@@ -1,29 +1,27 @@
-import {template} from '../tendersTemplate';
-import io from 'socket.io-client';
-import {createErrorMessage} from "../error";
+import { template } from "../tendersTemplate";
+import io from "socket.io-client";
+import { createErrorMessage } from "../error";
 
 const socket = io.connect("http://194.87.236.64:8082");
 
 let connectErrorCount = 0;
 
-socket.on('connect_error', () => {
-  console.log('Connection Failed');
+socket.on("connect_error", () => {
+  console.log("Connection Failed");
   connectErrorCount++;
 
   if (connectErrorCount >= 5) {
     socket.disconnect();
-    console.log('stop reconection');
+    console.log("stop reconection");
   }
 });
 
-
-socket.on('reconnect', () => {
-  console.log('reconnect');
+socket.on("reconnect", () => {
+  console.log("reconnect");
   connectErrorCount = 0;
 });
 
-
-socket.on('message', (data) => {
+socket.on("message", (data) => {
   const parseData = JSON.parse(data);
 
   parseData.message && createErrorMessage(parseData.message);
