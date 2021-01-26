ab04bcf1836e45ef8465b43cd3cf263ecfd1a696
dgekaa <dgeka_vip@mail.ru>
2021-01-26 18:37:13 +0300

    proxy

--- front/src/assets/scripts/sockets/index.js
+++ front/src/assets/scripts/sockets/index.js
@@ -2,7 +2,8 @@ import { template } from "../tendersTemplate";
 import io from "socket.io-client";
 import { createErrorMessage } from "../error";
 
-const socket = io.connect("http://194.87.236.64:8082", {
+// const socket = io.connect("ws://localhost/ws", {
+const socket = io.connect("ws://194.87.236.64:8082", {
   upgrade: false,
   transports: ["websocket"],
 });
