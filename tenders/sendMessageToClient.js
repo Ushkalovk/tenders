0328e54205a0b26d1391bf1592520b6be1d2c475
UshkalovK <private.ushkalovk@gmail.com>
2021-04-22 12:39:18 +0300

    Update sendMessageToClient.js

--- tenders/sendMessageToClient.js
+++ tenders/sendMessageToClient.js
@@ -7,6 +7,7 @@ io.on('connection', (socket) => {
 
 module.exports = (data) => {
     for (const client of clients) {
+        console.log("Data= ",data)
         client.emit('message', JSON.stringify(data));
     }
 };
