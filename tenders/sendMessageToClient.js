812cfd1a3a478ea3c8c74555014eed41f8959bcb
UshkalovK <private.ushkalovk@gmail.com>
2021-04-23 14:12:44 +0300

    Update sendMessageToClient.js

--- tenders/sendMessageToClient.js
+++ tenders/sendMessageToClient.js
@@ -5,11 +5,23 @@ io.on('connection', (socket) => {
     clients.add(socket);
 });
 
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+      return value;
+    };
+  };
 
 module.exports = (data) => {
     for (const client of clients) {
         console.log("Data= ",data)
-        client.emit('message', JSON.stringify(data));
+        client.emit('message', JSON.stringify(data,getCircularReplacer() ));
     }
 };
 
