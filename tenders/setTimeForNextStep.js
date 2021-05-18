93b5da2f023ada14912dd8c45848f1d1001a5be6
UshkalovK <private.ushkalovk@gmail.com>
2021-05-18 14:44:14 +0300

    Update setTimeForNextStep.js

--- tenders/setTimeForNextStep.js
+++ tenders/setTimeForNextStep.js
@@ -3,24 +3,17 @@ const sendMessageToClient = require("./sendMessageToClient");
 
 
 module.exports = ({timer, ms, link}) => {
-    Tender.findOne({'link': link}, ()=>{
-        try{
-        async (err, tender) => {
-            if (err)
-                console.log(err);
+    Tender.findOne({'link': link}, async (err, tender) => {
+        if (err)
+            console.log(err);
 
-            tender.timeForNextStep = timer;
-            tender.timeForNextStepMs = ms;
+        tender.timeForNextStep = timer;
+        tender.timeForNextStepMs = ms;
 
-            tender.save(err => {
-                if (err) throw err;
+        tender.save(err => {
+            if (err) throw err;
 
-                sendMessageToClient({timer, ms, link});
-            });
-            }
-        }  
-        catch(e){
-            console.log(e, "Error")
-        }
-    });
+            sendMessageToClient({timer, ms, link});
+        });
+    })
 };
